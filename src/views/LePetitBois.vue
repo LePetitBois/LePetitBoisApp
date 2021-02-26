4c97cdc6373db9b6a9c32cdfd1d633baa5c33781
LePetitBois <clochard.matthieu+petitbois@gmail.com>
2021-02-26 21:44:45 +0000

    :sparkles: Create a new page for us to work on
    Set us up to start working on le petit bois.

---
+++ src/views/LePetitBois.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="le-petit-bois">
+    <h1>Le petit bois</h1>
+  </div>
+</template>
+
+<style lang="scss">
+
+.le-petit-bois {
+  color: green;
+  background-color: brown;
+}
+</style>
